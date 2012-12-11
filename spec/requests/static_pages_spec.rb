e6a48e097a1104a811730f3a2b06b6a9d9a9dd81
Robert <kolbobby@gmail.com>
2012-12-11 10:35:21 -0800

    Add full_title To Application Helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) { "Physical Education" }
 	describe "HomePage" do
 		it "should have the h1 'Phys. Ed.'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Phys. Ed.')
 		end
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => 'Physical Education | Home')
+			page.should have_selector('title', :text => "#{base_title}")
+		end
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => ' | Home')
 		end
 	end
 	describe "HelpPage" do
@@ -18,7 +23,7 @@ describe "StaticPages" do
 		end
 		it "should have the title 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => 'Physical Education | Help')
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 end
