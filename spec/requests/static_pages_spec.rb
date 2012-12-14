a51c2e511c3ea012423b143c63a4d6b64facf30c
Robert <kolbobby@gmail.com>
2012-12-14 10:14:11 -0800

    Editing GemFile and Gemfile.lock

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,25 +4,25 @@ describe "StaticPages" do
 	let(:base_title) { "Physical Education" }
 	describe "HomePage" do
 		it "should have the h1 'Phys. Ed.'" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('h1', :text => 'Phys. Ed.')
 		end
 		it "should have the base title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', :text => "#{base_title}")
 		end
 		it "should not have a custom page title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should_not have_selector('title', :text => ' | Home')
 		end
 	end
 	describe "HelpPage" do
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
