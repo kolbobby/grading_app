64983144cd82f46078086d4b666e34fc447a0fc5
Robert <kolbobby@gmail.com>
2012-12-18 09:28:33 -0800

    Finish general layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	let(:base_title) { "Physical Education" }
 	subject { page }
 
 	describe "HomePage" do
 		before { visit root_path }
+
 		it { should have_selector('h1', :text => 'Phys. Ed.') }
 		it { should have_selector('title', full_title('')) }
 		it { should_not have_selector('title', :text => ' | Home') }
 	end
 	describe "HelpPage" do
 		before { visit help_path }
+
 		it { should have_selector('h1', :text => 'Help') }
 		it { should have_selector('title', full_title('Help')) }
 	end
