d2c4942eba72c505cc40deec1ac9bf6071853627
Robert <kolbobby@gmail.com>
2012-12-19 10:37:08 -0800

    Editing TDD

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "UserPages" do
 		let(:user) { FactoryGirl.create(:user) }
 		before { visit user_path(user) }
 
-		it { should have_selector('h1', :text => user.name) }
+		it { should have_selector('h3', :text => user.name) }
 		it { should have_selector('title', :text => user.name) }
 	end
 end
