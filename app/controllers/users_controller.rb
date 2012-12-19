5a0d11b443018b9802ef83cd35af4bba25e45220
Robert <kolbobby@gmail.com>
2012-12-18 19:23:34 -0800

    Error page for non-existent student

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		if @user.nil?
-			render :action => 'static_pages#index'
+			render :nothing => true
 		end
 	end
 end
