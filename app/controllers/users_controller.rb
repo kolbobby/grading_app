0505be75ed3025c1c2bba4cb7d75d7187e4b3575
Robert <kolbobby@gmail.com>
2013-01-02 10:15:52 -0800

    Editing users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,4 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
-
-	def new
-		@user = User.new
-	end
-
-	def create
-		@user = User.new(params[:user])
-		if @user.save
-			flash[:success] = "Added user to database"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
 end
