870dc004e161659ce0259443e654fe81b4603c79
Robert <kolbobby@gmail.com>
2013-01-04 09:48:41 -0800

    Adding type to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121221181929) do
+ActiveRecord::Schema.define(:version => 20130104174634) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20121221181929) do
     t.string   "uname"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.integer  "type"
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
