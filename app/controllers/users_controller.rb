e755ed9bb3304e980934d752afe3aac4906e153b
Robert <kolbobby@gmail.com>
2013-01-11 10:21:44 -0800

    Working on viewing all users via RJQuery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, :only => [:view_students]
+
 	def show
 		@user = User.find(params[:id])
 	end
 
 	def view_students
 		respond_to do |format|
+			format.html { @users = User.all }
 			format.js { render :layout => false }
 		end
 	end
