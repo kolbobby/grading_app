5d5d94dd6962ef78196a16a3d71ad7a155c74f91
Robert <kolbobby@gmail.com>
2013-01-11 09:38:28 -0800

    Adding view students using RJQuery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def view_students
+		respond_to do |format|
+			format.js { render :layout => false }
+		end
+	end
 end
