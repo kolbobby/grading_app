3dab5325fb1c7e56d9159416eee9f7642e347aab
Robert <kolbobby@gmail.com>
2013-01-12 19:42:18 -0800

    Fixing user side bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,16 @@ class UsersController < ApplicationController
 			format.js { render :layout => false }
 		end
 	end
+
+	private
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_path, :notice => "Please sign in."
+			end
+		end
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
 end
