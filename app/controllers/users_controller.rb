94f846af29b301fcfe3493275325af2f910e1c56
Robert <kolbobby@gmail.com>
2013-01-13 17:33:25 -0800

    Adding the the user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,12 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def view_home
+		respond_to do |format|
+			format.js { render :layout => false }
+		end
+	end
+
 	def view_students
 		@users = User.all
 		respond_to do |format|
