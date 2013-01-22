c101c4b0bde4e8b60b7fdc0ea967b920632e6e78
Robert <kolbobby@gmail.com>
2013-01-22 09:42:04 -0800

    Adding bulk student add-in

--- app/assets/javascripts/bulk_add_jquery.js
+++ app/assets/javascripts/bulk_add_jquery.js
@@ -26,7 +26,7 @@ $('#confirm').click(function() {
 });
 
 function add_list() { // VARIABLE: list
-	alert(document.getElementById('file_select').files[0]);
+	alert(document.getElementById('file_select').files[0].name);
 	/*$.ajax({
 		type: "GET",
 		url: list,
