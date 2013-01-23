2f6a990cdfd2215dc0edc08d60b575013998b657
Robert <kolbobby@gmail.com>
2013-01-23 13:20:16 -0800

    Working on bulk add-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 	end
 	def add_bulk_users_to_db
 		users = params[:users]
-		flash[:success] = "#{users[0]}"
+		flash[:success] = "#{users[0][0]}"
 	end
 
 	private
