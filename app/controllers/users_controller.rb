8930f5e5e9002a30f1912d208af64685086fe2ed
Robert <kolbobby@gmail.com>
2013-01-23 20:24:04 -0800

    Working on bulk add-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 			user = users[u].split(/,/)
 			@new_user = User.new(:name => user[0], :uname => user[0], :period => user[1], :grade_level => user[2], :sign_in_teacher => user[3], :password => "foobar", :password_confirmation => "foobar")
 			if @new_user.save
-				flash[:success] = "User #{@user[:name]} was added to the database"
+				flash[:success] = "User #{@new_user[:name]} was added to the database"
 			end
 		end
 	end
