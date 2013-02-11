ee3cedcc95438c8f98792696e3a3e82b283c098d
Robert <kolbobby@gmail.com>
2013-02-11 10:32:10 -0800

    Working on add activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,8 @@
 class ActivitiesController < ApplicationController
 	def add
-		#@coach = params[:coach]
+		@coach = params[:coach]
 		respond_to do |format|
 			format.js { render :layout => false }
 		end
-		return params[:coach]
 	end
 end
