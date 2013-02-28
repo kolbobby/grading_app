5592ecf2707e2d96f56d7baf6de449bd7b90e3a1
Robert <kolbobby@gmail.com>
2013-02-28 09:17:31 -0800

    Working on add activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -42,9 +42,9 @@ class ActivitiesController < ApplicationController
 			end
 			activity = Activity.new(:activity_number => count, :coach => coach, :marking_period => mp, :name => a)
 			if activity.save
-				str = "#{str}#{count}: saved\n"
+				str = "#{str}#{mp}, #{count}: saved\n"
 			else
-				str = "#{str}#{count}: not saved\n" 
+				str = "#{str}#{mp}, #{count}: not saved\n" 
 			end
 			count = count + 1
 		end
