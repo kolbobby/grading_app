db2ac59bc503b743c63611c47744e9b361381612
Robert <kolbobby@gmail.com>
2013-03-01 09:20:50 -0800

    Working on add activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -34,36 +34,33 @@ class ActivitiesController < ApplicationController
 		str = ""
 
 		existing_activities = Activity.find(:all, :conditions => { :coach => coach })
-		str = "#{existing_activities.count}"
 		if existing_activities.count == 12
-			#count = 0
-			#existing_activities.each do |e|
-			#	str = "#{str}#{e.name}, #{e.update_act(activities[count])}\n"
-			#	count = count + 1
-			#end
-		#	str = "edit"
+			count = 0
+			existing_activities.each do |e|
+				str = "#{str}#{e.name}, #{e.update_act(activities[count])}\n"
+				count = count + 1
+			end
 		else
 			if existing_activities.count > 0
 				existing_activities.each do |e|
 					Activity.find(:first, :conditions => { :coach => coach, :name => e.name, :marking_period => e.marking_period }).destroy
 				end
 			end
-			#count = 1
-			#mp = 1
-			#activities.each do |a|
-			#	if count > 3
-			#		count = 1
-			#		mp = mp + 1
-			#	end
-			#	activity = Activity.new(:activity_number => count, :coach => coach, :marking_period => mp, :name => a)
-			#	if activity.save
-			#		str = "#{str}#{mp}, #{count}: saved\n"
-			#	else
-			#		str = "#{str}#{mp}, #{count}: not saved\n" 
-			#	end
-			#	count = count + 1
-			#end
-		#	str = "add"
+			count = 1
+			mp = 1
+			activities.each do |a|
+				if count > 3
+					count = 1
+					mp = mp + 1
+				end
+				activity = Activity.new(:activity_number => count, :coach => coach, :marking_period => mp, :name => a)
+				if activity.save
+					str = "#{str}#{mp}, #{count}: saved\n"
+				else
+					str = "#{str}#{mp}, #{count}: not saved\n" 
+				end
+				count = count + 1
+			end
 		end
 
 		render :text => str
