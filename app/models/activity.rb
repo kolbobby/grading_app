e672e899e9a4e8acc053657820453fff4b5b8ca6
Robert <kolbobby@gmail.com>
2013-03-01 08:24:50 -0800

    Working on add activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,8 @@
 class Activity < ActiveRecord::Base
   attr_accessible :activity_number, :coach, :marking_period, :name
+
+  def update_act(new_act)
+  	update_attribute :name, new_act
+  	render :text => "#{self.name}"
+  end
 end
