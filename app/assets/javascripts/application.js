484749288e7adf38d4318852d0ba44d55e8f04d4
Robert <kolbobby@gmail.com>
2013-04-18 09:52:57 -0700

    Working on bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require bootstrap-collapse
 //= require jquery.ui.draggable
 //= require jquery.ui.droppable
 //= require jquery.ui.resizable
