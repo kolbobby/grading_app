f63eed5d7d094fc2a92af34f4f5cffa5f9fd5a0b
Robert <kolbobby@gmail.com>
2013-04-26 08:17:52 -0700

    Working on teacher schedules

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,8 +67,7 @@ class UsersController < ApplicationController
 			4.times do |x|
 				schedule.push(params["#{t[:name]}_marking_period_#{(x+1)}"])
 			end
-			t[:schedule] = schedule.to_s
-			if t.save
+			if t.update_attributes(:schedule => schedule.to_s)
 				str = "#{str}saved\n"
 			else
 				str = "#{str}failed\n"
