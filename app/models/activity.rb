7bb3f5babf37d50e2fa712931d474ed5a3147110
Robert <kolbobby@gmail.com>
2013-05-01 16:06:38 -0700

    Working on activity capacity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,3 @@
 class Activity < ActiveRecord::Base
-  attr_accessible :activity_number, :coach, :marking_period, :name
+  attr_accessible :activity_number, :coach, :marking_period, :name, :capacity
 end
