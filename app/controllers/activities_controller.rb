a0ccf3a37da36f337b8fe4716b2f4f5655706265
Robert <kolbobby@gmail.com>
2013-05-23 09:36:56 -0700

    Working on roster style

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -36,6 +36,7 @@ class ActivitiesController < ApplicationController
 				student = Nokogiri::XML::Node.new "student", builder
 
 				student.add_child("<name>#{s}</name>")
+				student.add_child("<coach>#{params[:coach]}</coach>")
 				student.add_child("<period>#{params[:period]}</period>")
 				student.add_child("<activity>#{params[:activity]}</activity>")
 				student.add_child("<marking_period>#{params[:marking]}</marking_period>")
